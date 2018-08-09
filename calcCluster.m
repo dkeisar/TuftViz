680fb8b410b642d027eb15bf1d0b496dbf661da9
GiladAtias <gilad.atias@gmail.com>
2018-08-09 16:22:44 +0300

    updating clustering

--- calcCluster.m
+++ calcCluster.m
@@ -1,33 +1,47 @@
-function calcCluster(trainingSet,h,l, maxClusters, labelDistanceFactor)
+function calcCluster(trainingSet,h,l, maxClusters, labelDistanceFactor, useUnsupervised)
+    addpath(genpath("PRML-PRMLT-v1.7-1-g3f9d968"));
     %method = 'Distance';
     %subMethod = 'euclidean';
     
     maxClust = maxClusters;
     sz = size(trainingSet);
-    [labelCoef,~] = pca(trainingSet(:,3:sz(2)));
-    reduceSet = trainingSet(:,3:sz(2)) * labelCoef(:,1);
+    %reduceSet = [];
+    if(useUnsupervised)
+        [reduceSet, ~, ~, ~] = pca(trainingSet(:,3:sz(2)), 1);
+    else
+        [reduceSet] = calcPredictions(trainingSet);
+    end
     clusterSet = [trainingSet(:,1:2), reduceSet*labelDistanceFactor];
-    t = kmeans(clusterSet, maxClust);
+    [~, model, ~] = mixGaussEm(clusterSet, maxClust);
+    [label, ~] = mixGaussPred(clusterSet, model);
     flipped = trainingSet;
     flipped(:,2) = 1 - trainingSet(:,2);
     figure(maxClust);
-    scatter(flipped(:,1)*l,flipped(:,2)*h,10,t);
+    scatter(flipped(:,1)*l,flipped(:,2)*h,10,label);
     axis equal
     
-    if(maxClusters >= 10)
+    if(maxClusters <= 10)
         maxClust = maxClusters + 1;
-        t = kmeans(clusterSet, maxClust);
+        [~, model, ~] = mixGaussEm(clusterSet, maxClust);
+        [label, ~] = mixGaussPred(clusterSet, model);
         figure(maxClust);
-        scatter(flipped(:,1)*l,flipped(:,2)*h,10,t);
+        scatter(flipped(:,1)*l,flipped(:,2)*h,10,label);
         axis equal
     end
     
-    if(maxClusters > 1 || maxClusters >= 10)
+    if(maxClusters > 1 && maxClusters <= 10)
         maxClust = maxClusters - 1;
-        t = kmeans(clusterSet, maxClust);
+        [~, model, ~] = mixGaussEm(clusterSet, maxClust);
+        [label, ~] = mixGaussPred(clusterSet, model);
         figure(maxClust);
-        scatter(flipped(:,1)*l,flipped(:,2)*h,10,t);
+        scatter(flipped(:,1)*l,flipped(:,2)*h,10,label);
         axis equal
     end
+    
+end
+
+function [answer] = calcPredictions(trainingSet)
+    global MLhandel
+    localweightVector = MLhandel.weightVector .* MLhandel.weightVector;
     
 end
---
+++ clusterStepOne.m
@@ -0,0 +1,57 @@
+function [tuftSet,labeledTufts] = clusterStepOne(bw,labeled,I, useUnsupervised)
+WindAngle=imageTune.FlowAngle;
+%% main clustering func
+    %% choose rand image then segment and tune it
+    [h,l]=size(bw);
+    [tuftSet,xcenter,ycenter,graindata]=create_tuft_set(labeled,bw,WindAngle);
+    
+    global MLhandel
+    global minNumInCluster
+    global featureSelected
+    %clusterGrid=[];
+    %graindata = regionprops(labeled,'basic');
+    %create_grid_for_clustering(I,bw,graindata)
+    %     uiwait(gcf)
+    gridedindex = MLhandel.clusterGrid.gridindex;
+    [gridedimage] = creatgridfromdata(graindata,gridedindex);
+    [trainingSet] = buildGridTrainingSet(gridedimage, tuftSet, h, l);
+    %clusterGrid=[];
+    
+    labelDistanceFactor = 4;
+    labeledTufts = calcCluster(trainingSet,h,l, MLhandel.noMaxClusters, labelDistanceFactor, useUnsupervised);
+    
+    %%
+    
+    
+end
+
+function [trainingSet] = buildGridTrainingSet(gridedTufts, tuftSet, h, l)
+    trainingSetSize = size(gridedTufts);
+    trainingSet = zeros(trainingSetSize(1), trainingSetSize(2));
+    for i = 1:trainingSetSize(1)
+        for j = 1:trainingSetSize(2)
+            [data, valid] = getTuftDataByCentroid(tuftSet, gridedTufts(i,j), h, l);
+            if(valid)
+                trainingSet(i,j) = data;
+            end
+        end
+    end
+end
+
+function [tuftData, isValid] = getTuftDataByCentroid(tuftSet, centroid, h, l)
+    isValid = false;
+    tuftData=[];
+    sz = size(tuftSet);
+    for ii = 1: sz(1)
+        for jj=1:sz(2)
+            tuft = tuftSet(ii,jj);
+            if(tuft.pixelX*h == centroid(1) && tuft.pixelY*l == centroid(2))
+                tuftData = [tuft.pixelX, tuft.pixelY, tuft.windRelatedAngle,...
+                    tuft.straightness, tuft.edgeRelatedrealAngle, tuft.length,...
+                    tuft.neighbor_1, tuft.neighbor_2, tuft.neighbor_3, tuft.neighbor_4];
+                isValid = true;
+                return;
+            end
+        end
+    end
+end
